bbb130b507d223ca143227b5ccdcc6b228407eb1
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2020-06-25 02:41:42 +0000

    Remove warning message
    The warning is as follows:
    Warning: 'classifiers' should be a list, got type 'tuple'
    
    Reference:	https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         packages=find_packages(exclude=['*tests*']),
         install_requires=install_requires,
         dependency_links=dependency_links,
-        classifiers=(
+        classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
@@ -78,5 +78,5 @@ if __name__ == '__main__':
             'Operating System :: POSIX',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-        )
+        ]
     )
